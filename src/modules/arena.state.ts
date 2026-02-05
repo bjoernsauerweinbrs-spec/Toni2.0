b3559e77d3b288791586908e791aeb0828fc5c0d
bjoernsauerweinbrs-spec <bjoern.sauerwein.brs@gmail.com>
2026-02-05 17:44:00 +0100

    Create arena.state.ts

---
+++ src/modules/arena.state.ts
@@ -0,0 +1,25 @@
+export interface ArenaPlayerPosition {
+  id: string
+  x: number
+  y: number
+}
+
+export interface ArenaLine {
+  id: string
+  type: 'line' | 'arrow'
+  color: string
+  start: { x: number; y: number }
+  end: { x: number; y: number }
+}
+
+export interface ArenaState {
+  field: string
+  players: ArenaPlayerPosition[]
+  lines: ArenaLine[]
+}
+
+export const arenaState: ArenaState = {
+  field: 'standard',
+  players: [],
+  lines: []
+}
